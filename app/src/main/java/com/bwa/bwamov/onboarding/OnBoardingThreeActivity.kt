77f43bc33f632c0be821aafc1f63cb380fd4b261
GabrielYoda <yodaegan@gmail.com>
2021-07-04 22:23:31 +0700

    add onBoarding Desain and backup zip

---
+++ app/src/main/java/com/bwa/bwamov/onboarding/OnBoardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.bwa.bwamov.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bwa.bwamov.R
+
+class OnBoardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_three)
+    }
+}
